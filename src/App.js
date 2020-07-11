4d3eca16895e37e660fbfdb1330a4d2e9a1db125
jasonbringle <jasonbringle@gmail.com>
2020-07-11 08:37:43 -0700

    built out state and handlers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [ info, setInfo] = useState({
+    name:'',
+    bio:""
+  })
+  
+  const changeHandler = e => {
+    setInfo({
+      ...info,
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  const submitHandler = e => {
+    e.preventDefault();
+    axios
+    .post("http://localhost:8000/api/users")
+    .then(res => console.log(res))
+    .catch(err => console.log("Error", err.message))
+  }
+
   return (
-    <div className="App">
+    <div className="App" onSubmit={submitHandler}>
       <form >
-        <input name='name'/>
-        <input name='bio'/>
+        <input type='text' name='name' placeholder="Name" value={info.name}onChange={changeHandler}/>
+        <input type='text' name='bio' placeholder="Bio" value={info.bio} onChange={changeHandler}/>
         <button>submit</button>
       </form>
       
